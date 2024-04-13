a78c71acd476f5a914d0624d9715e6d9a7ad6908
mvtvn78 <mvtvn78@gmail.com>
2024-04-13 14:44:18 +0700

    first commit

---
+++ src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author PC
+ */
+public class HelloWorld
+{
+  
+  
+    public static void main (String[] args)
+    {
+       int x= 5;
+       int y = 10;
+        System.out.println(x+ "Hello WOrld"+ y);
+    }
+
+}
